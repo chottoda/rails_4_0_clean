c3751ea7308546c6cd9a2f21b3117f2c43a626da
chottoda <Yasutoshi.Hiroe@jrits.ricoh.co.jp>
2015-12-18 11:07:40 +0900

    bundle exec rails new webapp --skip-bundle

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webapp::Application.config.secret_key_base = '2b4090ad0667562301dd9645119f54d9e8d64e11b8e77fbe540a2da63d171ac0ce0a9903acd5e6d7c3da29f5253daea869dd31c5fba957e756fdf7343dda2764'
